1b771688d49ea956c9c4b3c30199bfe30a7db6d3
himanshugithub <himanshu.er2025@gmail.com>
2024-10-15 18:20:16 +0530

    Implemented Header Component

--- frontend_smart_agri/smart-agriculture/src/components/Header.jsx
+++ frontend_smart_agri/smart-agriculture/src/components/Header.jsx
@@ -5,40 +5,45 @@ import axios from 'axios';
 
 
 
-useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-  
-const Header = () => (
-   
 
-  <div className="my-10 mx-2">
+
+const Header = () => {
+//     const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+//   useEffect(() => {
+//     const timerId = setInterval(() => {
+//       setTime(new Date().toLocaleTimeString());
+//     }, 1000);
+
+//     return () => clearInterval(timerId);},[]); // Cleanup
+   
+return(
+    
+    <div className="my-10 mx-2 w-[100%]">
     <div className="w-100 flex p-1  bg-[rgb(56,59,60)] text-white">
-      <h1 className="font-bold p-2">Smart Agriculture</h1>
-      <div className="p-2 flex">
-        <div className="bg-white text-[rgb(56,59,60)] ">Controller</div>
-        <div className="">FarmerDashboard</div>
-        <div className="">BuyerDashbord</div>
-        <div className="">MarketPlace</div>
-        <div className="">Alerts</div>
+      <h1 className="font-bold p-2 w-[15%]">Smart Agriculture</h1>
+      <div className="p-2 flex font-thin w-[60%] ">
+        <h2 className="bg-white text-[rgb(56,59,60)] p-1">Controller</h2>
+        <h2 className="p-1 text-[rgb(152,154,156)]">FarmerDashboard</h2>
+        <h2 className="p-1 text-[rgb(152,154,156)]">BuyerDashbord</h2>
+        <h2 className="p-1 text-[rgb(152,154,156)]">MarketPlace</h2>
+        <h2 className="p-1 text-[rgb(152,154,156)]">Alerts</h2>
       </div>
-      <div className="p-2 flex">
-        <div className="">Wind speed</div>
-        <div className="">Weather</div>
-        <div className="">Humidity</div>
-        <div className="">Time</div>
+      <div className="m-2 flex font-thin mx-auto border-[1px] border-[rgb(152,154,156)] bg">
+        <h2 className="p-1 text-end">Wind speed</h2>
+        <h2 className="p-1 text-end">Weather</h2>
+        <h2 className="p-1 text-end">Humidity</h2>
+        <h2 className="p-1 text-end">Current Time</h2>
         
       </div>
     </div>
-    <div className="w-100"></div>
-    <div className="w-100"></div>
+    <div className="w-[100%]"></div>
+    <div className="w-[100%]"></div>
   </div>
+    
+    
 );
+  
+};
 
 export default Header;
